a003bdfa5ae296a9c57b80b4a4485171c2308058
DorDaniel1989 <danielovelleiro@outlook.es>
2022-10-03 19:24:38 +0200

    first commit

---
+++ app/src/main/java/com/dani/universidades/MainActivity.java
@@ -0,0 +1,62 @@
+package com.dani.universidades;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button boton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        boton = (Button) findViewById(R.id.boton);
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LeerApi();
+            }
+        });
+    }
+
+    private void LeerApi() {
+
+        String url = "https://jsonplaceholder.typicode.com/posts/11";
+
+        StringRequest postResquest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+                    //text.setText(jsonObject.getString("userId"));
+                    String title = jsonObject.getString("title");
+                   System.out.println(title);
+                    //text.setText(jsonObject.getString("body"));
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("Error", error.getMessage());
+            }
+        });
+        Volley.newRequestQueue(this).add(postResquest);
+    }
+}
